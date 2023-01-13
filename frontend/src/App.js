3e7451cc53c5c40cb0732f0ca0fdd4ecb25c32ba
Kaleab Yakob <kaleab1234yakob@gmail.com>
2023-01-12 17:29:44 -0800

    eslint and prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Search from './components/Search';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
+
+
 const App = () => {
   const [word, setWord] = useState('');
 
@@ -14,7 +16,7 @@ const App = () => {
     fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
     .then((res) => res.json())
     .then((data) => {console.log(data);})
-    .catch((err) => {console.log(err);})
+    .catch((err) => {console.log(err);});
     setWord('');
   }
   
