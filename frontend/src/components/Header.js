afc51bf8b64c1bed6a8af2dd163792c5085de35f
Kaleab Yakob <78443389+kaleab108@users.noreply.github.com>
2023-01-28 08:18:18 -0800

    Welcome component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
 const navbarStyle = {
-  backgroundColor: 'lightblue',
+  backgroundColor: '#000',
 };
 
 const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Logo alt={title} style={{ maxWidth: '12rem', maxHeight: '3rem' }} />
       </Container>
     </Navbar>
   );
